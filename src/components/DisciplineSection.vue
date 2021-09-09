fa919b9ed5b22ecb9bb6e71cce2d06a36966235a
Ľubomír Mlích <lubomir.mlich@gmail.com>
2021-09-09 20:58:38 +0200

    change stat and restriction state imports to lowercase (it's red in IDE anyway, but at least not shown as unused import)

--- src/components/DisciplineSection.vue
+++ src/components/DisciplineSection.vue
@@ -35,8 +35,8 @@ methods: {
 
 <script setup>
 import { statSectionMixin } from "./mixins/statSectionMixin";
-import Stat from "./Stat.vue";
-import RestrictionState from "./RestrictionState.vue";
+import stat from "./Stat.vue";
+import restrictionState from "./RestrictionState.vue";
 
 defineProps(["clan"]);
 </script>
