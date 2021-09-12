f311ca5e755a66038df54a30301a8611a18d9067
Ľubomír Mlích <lubomir.mlich@gmail.com>
2021-09-12 21:24:04 +0200

    refactoring

--- cypress/integration/attributes.js
+++ cypress/integration/attributes.js
@@ -17,47 +17,23 @@ describe('Attributes test', () => {
         cy.contains('Strength').next().children().next().next().should('have.class', 'fill')
         cy.contains('Strength').next().children().next().should('have.class', 'fill')
     })
-    it('When we click on 5th dot of Dexterity, only three dots are added', () => {                
-        cy.contains('Dexterity').next().children().last().click()
-        cy.contains('Dexterity').next().children().last().should('not.have.class', 'fill')
-        cy.contains('Dexterity').next().children().next().next().should('have.class', 'fill')        
-        cy.contains('Dexterity').next().children().next().should('have.class', 'fill')
+    const lvl3Attrs = ["Dexterity", "Stamina", "Charisma"]
+    lvl3Attrs.forEach(element => {
+        it('When we click on 5th dot of ' + element + ', only three dots are allowed', () => {                
+            cy.contains(element).next().children().last().click()
+            cy.contains(element).next().children().last().should('not.have.class', 'fill')
+            cy.contains(element).next().children().next().next().should('have.class', 'fill')        
+            cy.contains(element).next().children().next().should('have.class', 'fill')
+        })
     })
-    it('When we click on 5th dot of Stamina, only three dots are added', () => {                
-        cy.contains('Stamina').next().children().last().click()
-        cy.contains('Stamina').next().children().last().should('not.have.class', 'fill')
-        cy.contains('Stamina').next().children().next().next().should('have.class', 'fill')
-        cy.contains('Stamina').next().children().next().should('have.class', 'fill')
-    })
-    it('When we click on 5th dot of Charisma, only three dots are added', () => {                
-        cy.contains('Charisma').next().children().last().click()
-        cy.contains('Charisma').next().children().last().should('not.have.class', 'fill')
-        cy.contains('Charisma').next().children().next().next().should('have.class', 'fill')
-        cy.contains('Charisma').next().children().next().should('have.class', 'fill')
-    })
-    it('When we click on 5th dot of Manipulation, only two dots are added', () => {                
-        cy.contains('Manipulation').next().children().last().click()
-        cy.contains('Manipulation').next().children().last().should('not.have.class', 'fill')
-        cy.contains('Manipulation').next().children().next().next().should('not.have.class', 'fill')
-        cy.contains('Manipulation').next().children().next().should('have.class', 'fill')
-    })
-    it('When we click on 5th dot of Composure, only one dot is added', () => {                
-        cy.contains('Composure').next().children().last().click()
-        cy.contains('Composure').next().children().last().should('not.have.class', 'fill')
-        cy.contains('Composure').next().children().next().next().should('not.have.class', 'fill')
-        cy.contains('Composure').next().children().next().should('have.class', 'fill')
-    })
-    it('When we click on 5th dot of Intelligence, only one dot is added', () => {                
-        cy.contains('Intelligence').next().children().last().click()
-        cy.contains('Intelligence').next().children().last().should('not.have.class', 'fill')
-        cy.contains('Intelligence').next().children().next().next().should('not.have.class', 'fill')
-        cy.contains('Intelligence').next().children().next().should('have.class', 'fill')
-    })
-    it('When we click on 5th dot of Wits, only one dot is added', () => {                
-        cy.contains('Wits').next().children().last().click()
-        cy.contains('Wits').next().children().last().should('not.have.class', 'fill')
-        cy.contains('Wits').next().children().next().next().should('not.have.class', 'fill')
-        cy.contains('Wits').next().children().next().should('have.class', 'fill')
+    const lvl2Attrs = ["Manipulation", "Composure", "Intelligence", "Wits"]
+    lvl2Attrs.forEach(element => {
+        it('When we click on 5th dot of ' + element + ', only two dots are allowed', () => {                
+            cy.contains(element).next().children().last().click()
+            cy.contains(element).next().children().last().should('not.have.class', 'fill')
+            cy.contains(element).next().children().next().next().should('not.have.class', 'fill')        
+            cy.contains(element).next().children().next().should('have.class', 'fill')
+        })
     })
     it('When we click on 5th dot of Resolve, no dotis added', () => {                
         cy.contains('Resolve').next().children().last().click()
