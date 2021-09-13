c3eeda6793a83c24ee6d3fae0fe1449b291ee984
Ľubomír Mlích <lubomir.mlich@gmail.com>
2021-09-13 22:03:14 +0200

    resolve comments

--- cypress/integration/attributes.js
+++ cypress/integration/attributes.js
@@ -1,12 +1,12 @@
 describe('Attributes test', () => {
     const AttributeList = ["Strength","Dexterity", "Stamina", "Charisma", "Manipulation", "Composure", "Intelligence", "Wits", "Resolve"]
     const lvl3Attrs = ["Dexterity", "Stamina", "Charisma"]
-    const lvl2Attrs = ["Manipulation", "Composure", "Intelligence", "Wits"]
+    const lvl2Attrs = ["Manipulation", "Composure", "Intelligence", "Wits"]    
 
     it('All Attributes has initial one dot', () => {
-        cy.visit('http://localhost:3000')        
+        cy.visit('http://localhost:3000')
         AttributeList.forEach(element => {
-            cy.contains('Strength').next().children().first().should('have.class', 'init') 
+            cy.contains(element).next().children().first().should('have.class', 'init') 
         })
     })
     it('Click on Strength 1st dot will not add fill class or remove init class', () => {
@@ -22,36 +22,47 @@ describe('Attributes test', () => {
         cy.contains('Strength').next().children().next().first().click()
         cy.contains('Strength').next().children().next().first().should('not.have.class', 'fill')
     })
-    it('Click on Strength 5th point will not add fill class to the fith dot', () => {        
+    it('Click on Strength 5th point will not add fill class to the fifth dot', () => {        
         cy.contains('Strength').next().children().last().click()
         cy.contains('Strength').next().children().last().first().should('not.have.class', 'fill')
     })
-    it('But will add fill class to all previous dots', () => {                
+    it('But will add fill class to all previous dots aside from first', () => {                
         cy.contains('Strength').next().children().next().next().next().should('have.class', 'fill')
         cy.contains('Strength').next().children().next().next().should('have.class', 'fill')
         cy.contains('Strength').next().children().next().should('have.class', 'fill')
+        cy.contains('Strength').next().children().first().should('not.have.class', 'fill')
+        cy.contains('Strength').next().children().first().should('have.class', 'init')
     })
     lvl3Attrs.forEach(element => {
         it('When we click on 5th dot of ' + element + ', only three dots are allowed', () => {                
             cy.contains(element).next().children().last().click()
             cy.contains(element).next().children().last().should('not.have.class', 'fill')
+            cy.contains(element).next().children().next().next().next().should('not.have.class', 'fill')
             cy.contains(element).next().children().next().next().should('have.class', 'fill')        
             cy.contains(element).next().children().next().should('have.class', 'fill')
+            cy.contains(element).next().children().first().should('not.have.class', 'fill')
+            cy.contains(element).next().children().first().should('have.class', 'init')
         })
     })
     lvl2Attrs.forEach(element => {
         it('When we click on 5th dot of ' + element + ', only two dots are allowed', () => {                
             cy.contains(element).next().children().last().click()
             cy.contains(element).next().children().last().should('not.have.class', 'fill')
+            cy.contains(element).next().children().next().next().next().should('not.have.class', 'fill')
             cy.contains(element).next().children().next().next().should('not.have.class', 'fill')        
             cy.contains(element).next().children().next().should('have.class', 'fill')
+            cy.contains(element).next().children().first().should('not.have.class', 'fill')
+            cy.contains(element).next().children().first().should('have.class', 'init')
         })
     })
     it('When we click on 5th dot of Resolve, no dots added', () => {                
         cy.contains('Resolve').next().children().last().click()
         cy.contains('Resolve').next().children().last().should('not.have.class', 'fill')
+        cy.contains('Resolve').next().children().next().next().next().should('not.have.class', 'fill')
         cy.contains('Resolve').next().children().next().next().should('not.have.class', 'fill')
         cy.contains('Resolve').next().children().next().should('not.have.class', 'fill')
+        cy.contains('Resolve').next().children().first().should('not.have.class', 'fill')
+        cy.contains('Resolve').next().children().first().should('have.class', 'init')
     })
     it('When all dots are divided, we click on 1st dot of Strength and there will be only one dot there', () => {                
         cy.contains('Strength').next().children().first().click()
