89907d4d704e3fca0117cdf1bfbfaa83ce797341
Ľubomír Mlích <lubomir.mlich@gmail.com>
2021-11-07 21:21:26 +0100

    add whitespaces

--- cypress/integration/vitals.js
+++ cypress/integration/vitals.js
@@ -9,23 +9,23 @@ function nthChild(CyElement, Iterator) {
 }
 
 function getNthPointOf(Item, Number) {
-    return nthChild(cy.contains(Item).next().children(),Number)
+    return nthChild(cy.contains(Item).next().children(), Number)
 }
 
 function checkNumberOfPoints(Item, ExpectedInit, ExpectedFill, Scale) {
-    var CurrentItem=cy.contains(Item).next().children()
+    var CurrentItem = cy.contains(Item).next().children()
     var i = 1
     while ('UP DOG'){
-        if (i<=ExpectedFill) {
-            CurrentItem.first().should('have.class', (i<=ExpectedInit) ? 'init' : 'fill')
+        if (i <= ExpectedFill) {
+            CurrentItem.first().should('have.class', (i <= ExpectedInit) ? 'init' : 'fill')
         }
         else {
-            CurrentItem.first().should('not.have.class','fill').or('not.have.class','init')      
+            CurrentItem.first().should('not.have.class', 'fill').or('not.have.class', 'init')      
         }
         if (i !== Scale){
             break
         }
-        CurrentItem=CurrentItem.next()
+        CurrentItem = CurrentItem.next()
         i++
     }
 }
@@ -34,26 +34,26 @@ describe('Vitals test',()=>{
     it('Page Exists', () => {
         cy.visit('/')
     })
-    it('Initialization check of Stamina, Composure, Resolve, Health, Willpower ', () => {
+    it('Initialization check of Stamina, Composure, Resolve, Health, Willpower', () => {
         checkNumberOfPoints('Stamina', 1, 1, 5)
         checkNumberOfPoints('Composure', 1, 1, 5)
         checkNumberOfPoints('Resolve', 1, 1, 5)
-        checkNumberOfPoints('Health', 4, 4, 15) //health = stamina + 3
-        checkNumberOfPoints('Willpower', 2, 2, 15) //will = resolve + composure
+        checkNumberOfPoints('Health', 4, 4, 15) // health = stamina + 3
+        checkNumberOfPoints('Willpower', 2, 2, 15) // willpower = resolve + composure
     })
     it('Click on Stamina 4th dot will add fill class and Health will have added fill class up to 7th Point', () => {
-        getNthPointOf('Stamina',4).click()
+        getNthPointOf('Stamina', 4).click()
         checkNumberOfPoints('Stamina', 1, 4, 5)
-        checkNumberOfPoints('Health', 4, 7, 15) //health = stamina + 3
+        checkNumberOfPoints('Health', 4, 7, 15) // health = stamina + 3
     })
     it('Click on Composure 3rd dot will add fill class and Willpower shall have added fill class up to 4th Point', () => {
-        getNthPointOf('Composure',3).click()
+        getNthPointOf('Composure', 3).click()
         checkNumberOfPoints('Composure', 1, 3, 5)
-        checkNumberOfPoints('Willpower', 2, 4, 15) //will = resolve + composure
+        checkNumberOfPoints('Willpower', 2, 4, 15) // willpower = resolve + composure
     })
     it('Click on Resolve 3rd dot will add fill class and Willpower shall have added fill class up to 4th Point', () => {
-        getNthPointOf('Resolve',3).click()
+        getNthPointOf('Resolve', 3).click()
         checkNumberOfPoints('Resolve', 1, 3, 5)
-        checkNumberOfPoints('Willpower', 2, 6, 15) //will = resolve + composure
+        checkNumberOfPoints('Willpower', 2, 6, 15) // willpower = resolve + composure
     })
 })
