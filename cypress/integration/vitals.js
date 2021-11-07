ef2dbdc7941f4ea872c19e3db4b7cbf1a83a61a2
longnicknam3 <pavelmlich@gmail.com>
2021-11-07 14:04:08 +0100

    test successful and seems to work

---
+++ cypress/integration/vitals.js
@@ -0,0 +1,52 @@
+function nthChild(CyElement, Iterator) {
+    if (Iterator < 1 ){
+        return undefined
+    }
+    if (Iterator !== 1) {
+        return nthChild(CyElement.next(), Iterator - 1)
+    }
+    return CyElement.first()
+}
+
+function getNthPointOf(Item, Number) {
+    return nthChild(cy.contains(Item).next().children(),Number)
+}
+
+function checkNumberOfPoints(Item, ExpectedInit, ExpectedFill, Scale) {
+    for (var i = 1; i <= Scale; i++) {        
+        if (i<=ExpectedFill) {
+            getNthPointOf(Item,i).should('have.class', (i<=ExpectedInit) ? 'init' : 'fill')
+        }
+        else {
+            getNthPointOf(Item,i).should('not.have.class','fill').and('not.have.class','init')      
+        }     
+    }
+}
+
+describe('Vitals test',()=>{
+    it('Page Exists', () => {
+        cy.visit('/')
+    })
+    it('Initialization check of Stamina, Composure, Resolve, Health, Willpower ', () => {
+        checkNumberOfPoints('Stamina', 1, 1, 5)
+        checkNumberOfPoints('Composure', 1, 1, 5)
+        checkNumberOfPoints('Resolve', 1, 1, 5)
+        checkNumberOfPoints('Health', 4, 4, 15) //health = stamina + 3
+        checkNumberOfPoints('Willpower', 2, 2, 15) //will = resolve + composure
+    })
+    it('Click on Stamina 4th dot will add fill class and Health will have added fill class up to 7th Point', () => {
+        getNthPointOf('Stamina',4).click()
+        checkNumberOfPoints('Stamina', 1, 4, 5)
+        checkNumberOfPoints('Health', 4, 7, 15) //health = stamina + 3
+    })
+    it('Click on Composure 3rd dot will add fill class and Willpower shall have added fill class up to 4th Point', () => {
+        getNthPointOf('Composure',3).click()
+        checkNumberOfPoints('Composure', 1, 3, 5)
+        checkNumberOfPoints('Willpower', 2, 4, 15) //will = resolve + composure
+    })
+    it('Click on Resolve 3rd dot will add fill class and Willpower shall have added fill class up to 4th Point', () => {
+        getNthPointOf('Resolve',3).click()
+        checkNumberOfPoints('Resolve', 1, 3, 5)
+        checkNumberOfPoints('Willpower', 2, 6, 15) //will = resolve + composure
+    })
+})
