f0e986bf4fdceb4192c352b75dea495c22250bfc
longnicknam3 <pavelmlich@gmail.com>
2021-11-08 21:05:39 +0100

    zmeny/2

--- cypress/integration/vitals.js
+++ cypress/integration/vitals.js
@@ -44,17 +44,14 @@ describe('Vitals test',()=>{
     })
     it('Click on Stamina 4th dot will add fill class and Health will have added fill class up to 7th Point', () => {
         getNthPointOf('Stamina', 4).click()
-        checkNumberOfPoints('Stamina', 1, 4, 5)
         checkNumberOfPoints('Health', 4, 7, 15)
     })
     it('Click on Composure 3rd dot will add fill class and Willpower shall have added fill class up to 4th Point', () => {
         getNthPointOf('Composure', 3).click()
-        checkNumberOfPoints('Composure', 1, 3, 5)
         checkNumberOfPoints('Willpower', 2, 4, 15)
     })
     it('Click on Resolve 3rd dot will add fill class and Willpower shall have added fill class up to 4th Point', () => {
         getNthPointOf('Resolve', 3).click()
-        checkNumberOfPoints('Resolve', 1, 3, 5)
         checkNumberOfPoints('Willpower', 2, 6, 15)
     })
 })
