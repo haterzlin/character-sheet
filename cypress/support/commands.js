6c50458a0850752ad85c2436afab387b0278d1ea
Ľubomír Mlích <lubomir.mlich@gmail.com>
2021-11-15 21:12:19 +0100

    remove comments

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,14 +1,3 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-
-
 function nthChild(CyElement, Iterator) {
     if (Iterator < 1 ){
         return undefined
@@ -38,20 +27,3 @@ Cypress.Commands.add('checkNumberOfPoints', (Item, ExpectedInit, ExpectedFill, S
         }        
     }
 })
-
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
